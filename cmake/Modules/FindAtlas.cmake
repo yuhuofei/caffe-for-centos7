074be8a858e338281f4e2f5c3acdf0fa2b6d10a7
qingze <qingze>
2016-08-27 11:24:21 +0800

    modify FindAtlas.cmake for centos 7

--- cmake/Modules/FindAtlas.cmake
+++ cmake/Modules/FindAtlas.cmake
@@ -10,43 +10,44 @@
 #  Atlas_LIBRARYRARY_DIRS
 
 set(Atlas_INCLUDE_SEARCH_PATHS
-  /usr/include/atlas
-  /usr/include/atlas-base
-  $ENV{Atlas_ROOT_DIR}
-  $ENV{Atlas_ROOT_DIR}/include
+    /usr/include/atlas
+    /usr/include/atlas-base
+    $ENV{Atlas_ROOT_DIR}
+    $ENV{Atlas_ROOT_DIR}/include
+)
+
+set(Lapack_INCLUDE_SEARCH_PATHS
+    /usr/include/lapacke    
 )
 
 set(Atlas_LIB_SEARCH_PATHS
-  /usr/lib/atlas
-  /usr/lib/atlas-base
-  $ENV{Atlas_ROOT_DIR}
-  $ENV{Atlas_ROOT_DIR}/lib
-)
+    /usr/lib64/atlas
+    /usr/lib/atlas
+    /usr/lib/atlas-base
+    $ENV{Atlas_ROOT_DIR}
+    $ENV{Atlas_ROOT_DIR}/lib
+    )
 
 find_path(Atlas_CBLAS_INCLUDE_DIR   NAMES cblas.h   PATHS ${Atlas_INCLUDE_SEARCH_PATHS})
-find_path(Atlas_CLAPACK_INCLUDE_DIR NAMES clapack.h PATHS ${Atlas_INCLUDE_SEARCH_PATHS})
+find_path(Atlas_CLAPACK_INCLUDE_DIR NAMES clapack.h PATHS ${Lapack_INCLUDE_SEARCH_PATHS})
 
-find_library(Atlas_CBLAS_LIBRARY NAMES  ptcblas_r ptcblas cblas_r cblas       PATHS ${Atlas_LIB_SEARCH_PATHS})
-find_library(Atlas_BLAS_LIBRARY NAMES   atlas_r   atlas                       PATHS ${Atlas_LIB_SEARCH_PATHS})
-find_library(Atlas_LAPACK_LIBRARY NAMES lapack alapack_r alapack lapack_atlas PATHS ${Atlas_LIB_SEARCH_PATHS})
+find_library(Atlas_BLAS_LIBRARY NAMES  satlas tatlas PATHS ${Atlas_LIB_SEARCH_PATHS})
 
 set(LOOKED_FOR
-  Atlas_CBLAS_INCLUDE_DIR
-  Atlas_CLAPACK_INCLUDE_DIR
+    Atlas_CBLAS_INCLUDE_DIR
+    Atlas_CLAPACK_INCLUDE_DIR
 
-  Atlas_CBLAS_LIBRARY
-  Atlas_BLAS_LIBRARY
-  Atlas_LAPACK_LIBRARY
-)
+    Atlas_BLAS_LIBRARY
+    )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Atlas DEFAULT_MSG ${LOOKED_FOR})
 
 if(ATLAS_FOUND)
-  set(Atlas_INCLUDE_DIR ${Atlas_CBLAS_INCLUDE_DIR} ${Atlas_CLAPACK_INCLUDE_DIR})
-  set(Atlas_LIBRARIES ${Atlas_LAPACK_LIBRARY} ${Atlas_CBLAS_LIBRARY} ${Atlas_BLAS_LIBRARY})
-  mark_as_advanced(${LOOKED_FOR})
+    set(Atlas_INCLUDE_DIR ${Atlas_CBLAS_INCLUDE_DIR} ${Atlas_CLAPACK_INCLUDE_DIR})
+    set(Atlas_LIBRARIES ${Atlas_BLAS_LIBRARY})
+    mark_as_advanced(${LOOKED_FOR})
 
-  message(STATUS "Found Atlas (include: ${Atlas_CBLAS_INCLUDE_DIR}, library: ${Atlas_BLAS_LIBRARY})")
+    message(STATUS "Found Atlas (include: ${Atlas_CBLAS_INCLUDE_DIR}, library: ${Atlas_BLAS_LIBRARY})")
 endif(ATLAS_FOUND)
 
